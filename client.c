e0cd915dd430c32bea02c624477221a131904bdb
sabiden <sabiden@stuy.edu>
2017-01-20 15:24:01 -0500

    Update client.c

--- client.c
+++ client.c
@@ -22,16 +22,26 @@ int main( int argc, char *argv[] ) {
   //read( sd, buffer, sizeof(buffer) );
   
   char buffer[MESSAGE_BUFFER_SIZE];
+
+  //Reading for Setup
+  read( sd, buffer, sizeof(buffer) );
+  setPuzzle(buffer);
+  
   
   while (1) {
+    Display();
     printf("Enter Letter: ");
     fgets( buffer, sizeof(buffer), stdin );
     char *p = strchr(buffer, '\n');
     *p = 0;
-  
-    write( sd, buffer, sizeof(buffer) );
-    read( sd, buffer, sizeof(buffer) );
-    printf( "received: %s\n", buffer );
+
+    if(Player_Input(buffer)){
+    
+      write( sd, buffer, sizeof(buffer) );
+      read( sd, buffer, sizeof(buffer) );
+      //printf( "received: %s\n", buffer );
+    }
+    
   }
   
   return 0;
