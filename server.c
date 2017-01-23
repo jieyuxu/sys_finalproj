2ea44aa504ea22d41106b7468d298ebc3e812f43
jieyuxu <jieyu.stuy@gmail.com>
2017-01-22 23:39:44 -0500

    more multi client connecting

--- server.c
+++ server.c
@@ -13,31 +13,64 @@ void sub_server( int sd );
 
 int main() {
 
-  int x = 1;
-  int f;
-  int sd, connection;
+  // int x = 1;
+  // int f;
+  // int sd, connection;
 
-  char buffer[MESSAGE_BUFFER_SIZE];
+  // char buffer[MESSAGE_BUFFER_SIZE];
 
-  sd = server_setup();
+  int sd = server_setup();
   printf("Waiting to connect... \n");
 
+  // while (1) {
+  //   connection = server_connect( sd );
+  //   //printf("start game? (y/n)? ");
+  //   //fgets(buffer, sizeof(buffer), stdin);
+  //   // if (strchr(buffer, 'y')){
+  //   int pid = fork();
+  //   if ( pid == 0 ) {
+  //     close(sd);
+  //     sub_server( connection );
+  //     // printf("Reached this exit\n");
+  //     exit(0);
+  //   }        
+  //   else {
+  //     close( connection );
+  //   }
+  //   // }
+  // }
+  int newsockfd, portno;
+  unsigned int clilen;
+  char buffer[256];
+  struct sockaddr_in serv_addr, cli_addr;
+  int n, pid;
   while (1) {
-    connection = server_connect( sd );
-    //printf("start game? (y/n)? ");
-    //fgets(buffer, sizeof(buffer), stdin);
-    // if (strchr(buffer, 'y')){
-    int pid = fork();
-    if ( pid == 0 ) {
-      close(sd);
-      sub_server( connection );
-      printf("Reached this exit\n");
-    }        
-    else {
-      close( connection );
-    }
-    // }
-  }
+        newsockfd = accept(sd, (struct sockaddr *) &cli_addr, &clilen);
+      
+        if (newsockfd < 0) {
+           perror("ERROR on accept");
+           exit(1);
+        }
+        
+        /* Create child process */
+        pid = fork();
+      
+        if (pid < 0) {
+           perror("ERROR on fork");
+           exit(1);
+        }
+        
+        if (pid == 0) {
+           /* This is the client process */
+           close(sd);
+           sub_server(newsockfd);
+           exit(0);
+        }
+        else {
+           close(newsockfd);
+        }
+      
+  } /* end of while */
   return 0;
 }
 
