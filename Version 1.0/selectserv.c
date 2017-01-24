d4830ddf9cb3f7d2cc5875e6661a62e51d2c8a6f
Mia <mobrien@stuy.edu>
2017-01-24 02:30:40 -0500

    Update selectserv.c

--- Version 1.0/selectserv.c
+++ Version 1.0/selectserv.c
@@ -97,7 +97,8 @@ int main(int argc , char *argv[]){
     //accept the incoming connection
     addrlen = sizeof(address);
     clients = 0;
-
+	
+    title(); //displays title
     Setup();
 
     puts("Waiting for connections ...");
