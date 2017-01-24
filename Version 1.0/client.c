f338a72416560ca088e58c7be4ecd4cd70bf5a6a
Mia <mobrien@stuy.edu>
2017-01-24 01:34:14 -0500

    Update client.c

--- Version 1.0/client.c
+++ Version 1.0/client.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,8 +33,6 @@ int main( int argc, char *argv[] ) {
   
   
   
-  
-  
 
   while (checkWin()) {
     Display();
@@ -43,8 +40,7 @@ int main( int argc, char *argv[] ) {
     fgets( buffer, sizeof(buffer), stdin );
     char *p = strchr(buffer, '\n');
     *p = 0;
-   
-
+    
     
     
     if(Player_Input(buffer)){
@@ -58,71 +54,3 @@ int main( int argc, char *argv[] ) {
 
   return 0;
 }
-
-
-
-
-
-
-/* #include <stdio.h> */
-/* #include <stdlib.h> */
-/* #include <string.h> */
-/* #include <unistd.h> */
-
-/* #include "executioner.h" */
-/* #include "networking.h" */
-
-/* char ans[100]; */
-/* int grim_reaper = 0; */
-
-/* int main( int argc, char *argv[] ) { */
-
-/*   char *host; */
-/*   if (argc != 2 ) { */
-/*     printf("host not specified, connecting to 127.0.0.1\n"); */
-/*     host = "127.0.0.1"; */
-/*   } */
-/*   else */
-/*     host = argv[1]; */
-  
-/*   int sd, isR; */
-
-/*   sd = client_connect( host ); */
-
-/*   char sbuffer[MESSAGE_BUFFER_SIZE]; */
-/*   char rbuffer[MESSAGE_BUFFER_SIZE]; */
-/*   char BANK[25]; */
-  
-
-/*   //Reading for Setup */
-/*   read( sd, sbuffer, sizeof(sbuffer) ); */
-/*   // setPuzzle(buffer); */
-/*   setPuzzle(sbuffer); */
-
-/*   while (1) { */
-/*     Display(); */
-
-/*     printf("Enter Letter: "); */
-
-/*     if (fgets( sbuffer, sizeof(sbuffer), stdin) != NULL){ */
-/*       char *p = strchr(sbuffer, '\n'); */
-/*       *p = 0; */
-/*       send(sd, sbuffer, 200, 0); */
-/*       Player_Input(sbuffer); */
-/*     } */
-
-/*    // printf("where you at 1\n"); */
-/*     //Player_Input(buffer, cat); */
-
-/*     // if(Player_Input(buffer, cat)){ */
-/*     //   //printf("where you at 2\n"); */
-/*     //   write( sd, buffer, sizeof(buffer) ); */
-/*     //  // printf("where you at 3\n"); */
-/*     //   read( sd, buffer, sizeof(buffer) ); */
-/*     //   //printf( "received: %s\n", buffer ); */
-/*     //   //printf("where you at 4\n"); */
-/*     // } */
-    
-/*   }   */
-/*   return 0; */
-/* } */
